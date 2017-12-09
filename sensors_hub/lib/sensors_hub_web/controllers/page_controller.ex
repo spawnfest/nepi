f01c00efcd1fdda11748dbf4c8e1d61e7b353348
Łukasz Skotarek <lukasz.skotarek@dgc.se>
2017-12-09 17:54:12 +0100

    sensors in index

--- sensors_hub/lib/sensors_hub_web/controllers/page_controller.ex
+++ sensors_hub/lib/sensors_hub_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule SensorsHubWeb.PageController do
   use SensorsHubWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    hydrations = SensorsHub.Sensors.list_hydrations()
+    thermals = SensorsHub.Sensors.list_thermals()
+    proximities = SensorsHub.Sensors.list_proximities()
+    humidities = SensorsHub.Sensors.list_humidities()
+
+    render(conn, "index.html", hydrations: hydrations, thermals: thermals, proximities: proximities, humidities: humidities)
   end
 end
